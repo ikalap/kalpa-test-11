2ba884d4eb98dd87e6bb3d7b555c9569945a7d74
kalpa <476619022@qq.com>
2025-06-23 10:21:18 +0800

    Initial commit

---
+++ src/main/java/org/example/kalpatest1/KalpaTest1Application.java
@@ -0,0 +1,13 @@
+package org.example.kalpatest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalpaTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalpaTest1Application.class, args);
+    }
+
+}
